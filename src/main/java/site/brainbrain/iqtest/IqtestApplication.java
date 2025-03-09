ef177111211860572923fcb49e272aba326c62a4
reddevilmidzy <midzy0228@gmail.com>
2025-03-09 17:07:36 +0900

    feat: 프로젝트 초기 설정

---
+++ src/main/java/site/brainbrain/iqtest/IqtestApplication.java
@@ -0,0 +1,13 @@
+package site.brainbrain.iqtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IqtestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IqtestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
@@ -0,0 +1,13 @@
+package site.brainbrain.iqtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IqtestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
