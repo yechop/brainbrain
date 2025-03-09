338da3809edf45de79f6d8dff14da9fc878645ae
reddevilmidzy <midzy0228@gmail.com>
2025-03-09 17:43:36 +0900

    test: 실패 테스트 추가

--- src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
+++ src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
@@ -1,5 +1,7 @@
 package site.brainbrain.iqtest;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +10,7 @@ class IqtestApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertThat(true).isFalse();
 	}
 
 }
