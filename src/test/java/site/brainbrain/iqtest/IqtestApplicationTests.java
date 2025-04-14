7be13af5352845484c8dc1fe36db477a62896d06
yechop <channetattoo@gmail.com>
2025-04-14 18:52:18 +0900

    test: test yml 생성

--- src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
+++ src/test/java/site/brainbrain/iqtest/IqtestApplicationTests.java
@@ -3,7 +3,7 @@ package site.brainbrain.iqtest;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-//@SpringBootTest
+@SpringBootTest
 class IqtestApplicationTests {
 
 	@Test
